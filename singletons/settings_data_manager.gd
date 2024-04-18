8ac82479b819cd106edf8fc972716f52944b3bdb
Mark Velez <mark.velez129@gmail.com>
2024-04-18 20:12:02 +0200

    Added display mode and resolution

---
+++ singletons/settings_data_manager.gd
@@ -0,0 +1,11 @@
+extends Node
+
+var SETTINGS_DATA: Dictionary = {
+	"gameplay": {},
+	"graphics": {
+		"displayMode": "Fullscreen",
+		"resolution": "1920x1080"
+	},
+	"controls": {},
+	"audio": {}
+}
