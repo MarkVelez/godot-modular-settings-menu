2d50cb57843a8f508a8272698794d897648aa2de
Mark Velez <mark.velez129@gmail.com>
2024-06-08 18:48:42 +0200

    Changed data saving

--- scripts/ui/settings_section.gd
+++ scripts/ui/settings_section.gd
@@ -18,6 +18,7 @@ var CHANGED_ELEMENTS: Array[StringName]
 func _ready():
 	# Connect neccessary signals from the central root node for the settings
 	settingsMenu.connect("get_settings", get_settings)
+	settingsMenu.connect("apply_settings", on_apply_settings)
 	settingsMenu.connect("clear_cache", clear_cache)
 	
 	# Add a reference of the section toe the reference table
@@ -45,31 +46,43 @@ func clear_cache() -> void:
 
 
 # Called to check for changes between the cache and the settings data
-func settings_changed(element: String) -> void:
+func settings_changed(element: StringName) -> void:
 	# Check if there are differences between the cache and the settings data
-	if SETTINGS_SECTION_CACHE == SettingsDataManager.SETTINGS_DATA[section]:
+	if SETTINGS_SECTION_CACHE[element] == SettingsDataManager.SETTINGS_DATA[section][element]:
 		settingsMenu.applyButton.set_disabled(true)
+		# Check if the element is on the changed elements list
+		if CHANGED_ELEMENTS.has(element):
+			# Remove the element from the list
+			CHANGED_ELEMENTS.erase(element)
 	else:
-		if not CHANGED_ELEMENTS.has(element):
-			CHANGED_ELEMENTS.append(element)
 		settingsMenu.applyButton.set_disabled(false)
+		# Check if the element is not the changed elements list
+		if not CHANGED_ELEMENTS.has(element):
+			# Add the element to the list
+			CHANGED_ELEMENTS.append(element)
 
 
 # Called to saved the data in the section's cache to the settings data and apply the settings to the game
 func on_apply_settings() -> void:
-	SettingsDataManager.SETTINGS_DATA[section] = SETTINGS_SECTION_CACHE.duplicate(true)
-	SettingsDataManager.save_data()
-	
-	for element in CHANGED_ELEMENTS:
-		ELEMENT_REFERENCE_TABLE[element].apply_settings()
-	
-	CHANGED_ELEMENTS.clear()
+	# Check if any of the sections elements have been changed
+	if CHANGED_ELEMENTS.size() > 0:
+		# Copy the section cache into the settings data dictionary
+		SettingsDataManager.SETTINGS_DATA[section] = SETTINGS_SECTION_CACHE.duplicate(true)
+		
+		# Apply the settings for the changed elements
+		for element in CHANGED_ELEMENTS:
+			ELEMENT_REFERENCE_TABLE[element].apply_settings()
+		
+		# Clear the changed elements array
+		CHANGED_ELEMENTS.clear()
 
 
 func discard_changes() -> void:
-	# Load the saved settings for each element in the section
-	for element in CHANGED_ELEMENTS:
-		ELEMENT_REFERENCE_TABLE[element].load_settings()
-	
-	# Clear the changed elements array
-	CHANGED_ELEMENTS.clear()
+	# Check if any of the sections elements have been changed
+	if CHANGED_ELEMENTS.size() > 0:
+		# Load the saved settings for each element in the section
+		for element in CHANGED_ELEMENTS:
+			ELEMENT_REFERENCE_TABLE[element].load_settings()
+		
+		# Clear the changed elements array
+		CHANGED_ELEMENTS.clear()
