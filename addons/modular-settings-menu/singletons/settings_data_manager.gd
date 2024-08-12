c55349be6a4959eb75ad135dbe3418313eb889fa
Mark Velez <mark.velez129@gmail.com>
2024-08-12 22:28:00 +0200

    Fixed typos

--- addons/modular-settings-menu/singletons/settings_data_manager.gd
+++ addons/modular-settings-menu/singletons/settings_data_manager.gd
@@ -1,21 +1,21 @@
 extends Node
-## Handles the loading and saving of settings data_.
+## Handles the loading and saving of settings data.
 
-## Emitted when loading the settings data_.
+## Emitted when loading the settings data.
 signal load_settings
 ## Emitted when applying specific settings to in game objects.
 signal apply_in_game_settings(section: StringName, setting: StringName, value)
 
 ## PATH to the settings save file.
 var DATA_FOLDER: String = OS.get_user_data_dir()
-## Name of the file that the settings data_ is saved in.
+## Name of the file that the settings data is saved in.
 const FILE_NAME: String = "settings"
 ## Extension for the save file.
 const FILE_EXTENSION: String = ".cfg"
 ## The PATH to the save file on the computer.
 var PATH: String = DATA_FOLDER + "/" + FILE_NAME + FILE_EXTENSION
 
-## Dictionary that stores all settings data_.
+## Dictionary that stores all settings data.
 var settingsData_: Dictionary
 ## A reference table of all sections.
 var SECTION_REFERENCE_TABLE_: Dictionary
@@ -37,7 +37,7 @@ func _ready() -> void:
 	
 	# Check if a save file exists
 	if FileAccess.file_exists(PATH):
-		# Proceed normally by retrieving data_ from the save file
+		# Proceed normally by retrieving data from the save file
 		call_deferred("get_data")
 	else:
 		# Enable the no save file flag
@@ -45,17 +45,17 @@ func _ready() -> void:
 		push_warning("No save file found")
 
 
-## Called to save the settings data_ to the save file.
+## Called to save the settings data to the save file.
 func save_data() -> void:
 	# Create a new config instance
 	var config := ConfigFile.new()
 	
-	# Add the data_ from the settings data_ dictionary
+	# Add the data from the settings data dictionary
 	for section in settingsData_:
 		for element in settingsData_[section]:
 			config.set_value(section, element, settingsData_[section][element])
 	
-	# Save the data_ to the specified directory
+	# Save the data to the specified directory
 	var err = config.save(PATH)
 	
 	# Check for errors
@@ -72,13 +72,13 @@ func save_data() -> void:
 		invalidSaveFile = false
 
 
-## Called to retrieve data_ from the save file.
+## Called to retrieve data from the save file.
 func get_data() -> void:
 	# Create a new config instance
 	var config := ConfigFile.new()
-	# Load the save data_
+	# Load the save data
 	var err := config.load(PATH)
-	# Temporary data_ dictionary
+	# Temporary data dictionary
 	var data_: Dictionary = {}
 	
 	# Check for errors
@@ -86,21 +86,21 @@ func get_data() -> void:
 		push_error("Failed to load settings data_: ", err)
 		return
 	
-	# Add the retrieved data_ to the settings data_ dictionary
+	# Add the retrieved data to the settings data dictionary
 	for section in config.get_sections():
 		data_[section] = {}
 		for key in config.get_section_keys(section):
 			data_[section][key] = config.get_value(section, key)
 	
-	# Verify the validity of the loaded data_
-	verify_settingsData_(data_)
+	# Verify the validity of the loaded data
+	verify_settings_data_(data_)
 	
-	# Copy the retrieved data_ into the settings data_ dictionary
+	# Copy the retrieved data into the settings data dictionary
 	settingsData_ = data_.duplicate(true)
 
 
 ## Checks if the save file has any invalid entries and removes them or adds missing sections.
-func verify_settingsData_(data_: Dictionary) -> void:
+func verify_settings_data_(data_: Dictionary) -> void:
 	# List of valid entries to compare to
 	var validEntries_: Dictionary = SECTION_REFERENCE_TABLE_.duplicate()
 	# Merge the element panel references into the valid entries
@@ -108,12 +108,12 @@ func verify_settingsData_(data_: Dictionary) -> void:
 	# List of invalid entries to be removed
 	var invalidEntries_: Dictionary = {}
 	
-	# Itterate through the loaded settings data_
+	# Itterate through the loaded settings data
 	for section in data_:
 		if is_valid_section(invalidEntries_, section):
 			verify_elements(data_, invalidEntries_, section)
 	
-	# Check if there are any sections missing from the retrieved data_
+	# Check if there are any sections missing from the retrieved data
 	check_for_missing_sections(data_, validEntries_)
 	
 	# Check if there are any invalid entries
@@ -123,7 +123,7 @@ func verify_settingsData_(data_: Dictionary) -> void:
 		remove_invalid_entries(data_, invalidEntries_, validEntries_)
 
 
-## Used by the verify_settingsData_() function to verify the retrieved sections.
+## Used by the verify_settings_data() function to verify the retrieved sections.
 func is_valid_section(invalidEntries_: Dictionary, SECTION: String) -> bool:
 	# Check if the section is in either of the reference tables
 	if (
@@ -138,7 +138,7 @@ func is_valid_section(invalidEntries_: Dictionary, SECTION: String) -> bool:
 	return false
 
 
-## Used by the verify_settingsData_() function to verify the elements inside of the retrieved sections.
+## Used by the verify_settings_data() function to verify the elements inside of the retrieved sections.
 func verify_elements(
 	data_: Dictionary,
 	invalidEntries_: Dictionary,
@@ -180,7 +180,7 @@ func get_valid_elements(SECTION: String) -> Array:
 	return []
 
 
-## Used by the verify_settingsData_() function to check if any expected sections are missing.
+## Used by the verify_settings_data() function to check if any expected sections are missing.
 func check_for_missing_sections(data_: Dictionary, validEntries_: Dictionary) -> void: 
 	# Itterate through all valid sections
 	for section in validEntries_:
@@ -193,7 +193,7 @@ func check_for_missing_sections(data_: Dictionary, validEntries_: Dictionary) ->
 			push_warning("Settings section is missing: ", section)
 
 
-## Used by the verify_settingsData_() function to remove invalid entires from the retrieved data_.
+## Used by the verify_settings_data() function to remove invalid entires from the retrieved data.
 func remove_invalid_entries(
 	data_: Dictionary,
 	invalidEntries_: Dictionary,
