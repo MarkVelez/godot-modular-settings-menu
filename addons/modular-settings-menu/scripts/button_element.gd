b8fd4dd41b8ce47ca031f414e5f556af23d3eb76
Mark Velez <mark.velez129@gmail.com>
2024-08-12 19:21:27 +0200

    Updated button element

--- addons/modular-settings-menu/scripts/button_element.gd
+++ addons/modular-settings-menu/scripts/button_element.gd
@@ -1,42 +1,40 @@
 extends Button
 
 # Reference to the element's panel
-@export var ElementPanel: PackedScene
+@export var ElementPanelScene: PackedScene
 
 # Reference to the node the settings element is under
-@onready var parentRef: Node = owner
-# Identifier for the element (used as the key for the section in the settings data)
-@onready var identifier: StringName = name
+@onready var ParentRef: Node = owner
 
 # Reference to the element's extra panel
-var elementPanelRef: Node
+var ElementPanelRef: Node
 
 
 func _ready():
 	# Connect necessary signals
-	connect(&"pressed", pressed)
+	connect("pressed", pressed)
 	create_element_panel()
 
 
 func pressed() -> void:
 	# Switch panels
-	parentRef.settingsMenu.settingsPanel.hide()
-	elementPanelRef.show()
+	ParentRef.SettingsMenuRef.SettingsPanelRef.hide()
+	ElementPanelRef.show()
 	# Populate the settings cache of the panel
-	elementPanelRef.get_settings()
+	ElementPanelRef.get_settings()
 
 
 # Called to create the element's extra panel
 func create_element_panel() -> void:
-	var elementPanels: Control = parentRef.settingsMenu.elementPanels
+	var elementPanelsRef: Control = ParentRef.SettingsMenuRef.ElementPanelsRef
 	# Instantiate the element panel
-	elementPanelRef = ElementPanel.instantiate()
+	ElementPanelRef = ElementPanelScene.instantiate()
 	
 	# Check if the element panel exists
-	if not elementPanels.find_child(elementPanelRef.name):
+	if not elementPanelsRef.find_child(ElementPanelRef.name):
 		# Give a reference of the element
-		elementPanelRef.panelOwner = self
-		elementPanelRef.hide()
+		ElementPanelRef.panelOwner = self
+		ElementPanelRef.hide()
 		# Add the panel to the element panels list
-		elementPanels.add_child(elementPanelRef)
-		elementPanelRef.set_owner(parentRef.owner)
+		elementPanelsRef.add_child(ElementPanelRef)
+		ElementPanelRef.set_owner(ParentRef.SettingsMenuRef)
